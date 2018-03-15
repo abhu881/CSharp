5a4e9da4b49a70a6fd75935ad2cf467550592fcb
Abhu West <abhu881@gmail.com>
2018-03-15 14:41:21 -0400

    adding Learning Tutorial and App Project

---
+++ LearningTutorial/LearningTutorial/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningTutorial
+{
+    public class Program
+    {
+        public Program() { }
+
+
+        private int addition;
+        public int Addition {
+            get { return addition; }
+            set { addition = value; }
+
+        }
+
+        int myarray = new int int[2] {1 ,2 };
+
+
+        public void Mymethod(int parameter1, string parameter2)
+        {
+            Console.WriteLine("First parameter {0} and secodn paramter {1}", parameter1, parameter2);
+        }
+
+
+        public Program()
+        {
+
+
+        }
+        static void Main(string[] args)
+        {
+            pub
+
+        }
+    }
+}
